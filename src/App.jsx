ced91bb6fa2b5167d17d4df467e3d7745dd6a5db
markopaunovic1 <marpau970514@hotmail.com>
2024-09-30 01:45:51 +0200

    fixed image with content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.jsx'
+import './App.css'
 import Header from './Components/Header/Header'
 import MainPage from './Components/Main/MainPage'
 
 function App() {
   return (
-   <div>
+   <div className= 'mainPage-adjustment'>
     <Header/>
     <MainPage/>
    </div>
--- src/Components/Main/MainPage.jsx
+++ src/Components/Main/MainPage.jsx
@@ -4,8 +4,14 @@ import './index.css'
 
 function MainPage() {
     return (
-        <div className="background-image">
-            <img src="https://www.kayak.co.uk/news/wp-content/uploads/sites/5/2023/08/THEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg"></img>
+        <div className="background-color">
+                <img src="https://www.kayak.co.uk/news/wp-content/uploads/sites/5/2023/08/THEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg" className="background-img"></img>
+            
+                <section class='hero-image-text'>
+                    <h1>FEEL THE DIFFERENCE</h1>
+                    <h3 className="h3-content">Memorable, elegant and significant - Polo Hotel creates meaningful experiences in inspiring environments.</h3>
+                    <button className="hero-button">READ MORE</button>
+            </section>   
         </div>
     );
 }
