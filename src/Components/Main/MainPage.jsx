c768c927a36402a4b0fbf7d856090889afd80036
markopaunovic1 <marpau970514@hotmail.com>
2024-09-30 13:47:56 +0200

    small changes and implements

--- src/Components/Main/MainPage.jsx
+++ src/Components/Main/MainPage.jsx
@@ -2,16 +2,27 @@ import React from "react"
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+function clickMe() {
+    
+}
+
 function MainPage() {
     return (
-        <div className="background-color">
+        <div className="mainPage-content-div">
                 <img src="https://www.kayak.co.uk/news/wp-content/uploads/sites/5/2023/08/THEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg" className="background-img"></img>
             
                 <section class='hero-image-text'>
                     <h1>FEEL THE DIFFERENCE</h1>
                     <h3 className="h3-content">Memorable, elegant and significant - Polo Hotel creates meaningful experiences in inspiring environments.</h3>
-                    <button className="hero-button">READ MORE</button>
-            </section>   
+                    <button onClick={clickMe}
+                     className="hero-button">
+                        READ MORE
+                        </button>
+                </section>
+
+                <h1 className="rooms-header-text">Rooms & Suites</h1>
+
+            
         </div>
     );
 }
