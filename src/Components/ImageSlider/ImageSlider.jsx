9d3f27c805fdd753a28c967b65a18b36687b3f38
markopaunovic1 <marpau970514@hotmail.com>
2024-10-07 02:35:51 +0200

    added buttons to scroll through images

--- src/Components/ImageSlider/ImageSlider.jsx
+++ src/Components/ImageSlider/ImageSlider.jsx
@@ -5,35 +5,73 @@ import hotelImages from '../../data/images'
 
 function ImageSlider() {
 
-    const [currentIndex, setCurrentIndex] = useState(0)
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
+
     const randomImages = hotelImages.randomRoomImages
 
     const carouselInfiniteScroll = () => {
         if (currentIndex === randomImages - 1) {
-            return setCurrentIndex(0)
+            return setCurrentIndex(0);
         }
-        return setCurrentIndex(currentIndex + 1)
+        return setCurrentIndex(currentIndex + 1);
     }
 
     useEffect(() => {
         const interval = setInterval(() => { carouselInfiniteScroll()}, 3000)
         // clean up function
-        return () => clearInterval(interval)
-    })
+        return () => clearInterval(interval);
+    });
+
+    const nextImage = () => {
+        setActiveIndex((val) => {
+            if (val >= randomImages.length -1) {
+                return setCurrentIndex(0);
+            } else {
+                return setCurrentIndex(currentIndex + 1);
+            }
+        });
+    }
+
+    const prevImage = () => {
+        setActiveIndex((val) => {
+            if (val <= 0)  {
+                return setCurrentIndex(0);
+            } else {
+                return setCurrentIndex(currentIndex - 1);
+            }
+        });
+    }
 
     return (
         <div className="carousel-container">
             {randomImages.map((image, index) => {
                 return (
                     <img 
-                        src={image.imgURL} 
+                        src={image.imgURL}
                         className="carousel-item"
                         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-                        key={index} 
+                        key={index}
                         alt={`Slide ${index}`}
                     />
                 );
             })}
+
+            <div className="button-container">
+                <button className="slider-button-next"
+                onClick={(e) => {
+                    e.preventDefault();
+                    nextImage();
+                }}
+                > {">"}</button>
+
+                <button className="slider-button-prev"
+                onClick={(e) => {
+                    e.preventDefault();
+                    prevImage();
+                }}
+                >{"<"}</button>
+            </div>
         </div>
     );
 } 
