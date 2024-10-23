ab34b301ea29a4d13a48c7af5da82a470d3da8ed
markopaunovic1 <marpau970514@hotmail.com>
2024-10-24 01:11:35 +0200

    fixed minor issue with css

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import ReactCalendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
 
-const valuePiece = Date();
-
-const valueer = valuePiece;
-
-function Calendar(onChange, value) {
+function Calendar() {
    // const [value, onChange] = useState<valueer>(new Date());
 
     return (
-        <div>
+        <div className="calendar">
             <ReactCalendar minDate={new Date()} onClickDay={(date) => console.log(date)}/>
         </div>
     )
--- src/Components/Main/MainPage.jsx
+++ src/Components/Main/MainPage.jsx
@@ -15,16 +15,17 @@ function MainPage() {
     return (
         <div className="mainPage-content-div">
             <Calendar />
+            <div className="container-image-text">
                 <img src="https://www.kayak.co.uk/news/wp-content/uploads/sites/5/2023/08/THEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg" className="background-img"></img>
-            
-                <section class='hero-image-text'>
-                    <h1>FEEL THE DIFFERENCE</h1>
-                    <h3 className="h3-content">Memorable, elegant and significant - Polo Hotel creates meaningful experiences in inspiring environments.</h3>
-                    <button onClick={clickMe}
-                     className="hero-button">
-                        READ MORE
+                    <div class='hero-image-text'>
+                        <h1>FEEL THE DIFFERENCE</h1>
+                        <h3 className="h3-content">Memorable, elegant and significant - Polo Hotel creates meaningful experiences in inspiring environments.</h3>
+                        <button onClick={clickMe}
+                        className="hero-button">
+                            READ MORE
                         </button>
-                </section>
+                    </div>
+                </div>
 
                 <div>
                     <h1 className="rooms-header-text">Rooms & Suites</h1>
